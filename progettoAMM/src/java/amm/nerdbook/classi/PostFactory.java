6add6160a857fcf15c0ce4caec8702ec51ba200e
Laura <Laura@Laura>
2017-04-22 12:36:39 +0100

    milestone3

--- progettoAMM/src/java/amm/nerdbook/classi/PostFactory.java
+++ progettoAMM/src/java/amm/nerdbook/classi/PostFactory.java
@@ -4,11 +4,85 @@
  * and open the template in the editor.
  */
 package amm.nerdbook.classi;
-
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author Laura
  */
 public class PostFactory {
-    
+
+    //Pattern Design Singleton
+    private static PostFactory singleton;
+
+    public static PostFactory getInstance() {
+        if (singleton == null) {
+            singleton = new PostFactory();
+        }
+        return singleton;
+    }
+
+    private ArrayList<Post> listaPost = new ArrayList<Post>();
+
+    private PostFactory() {
+        
+        GattoFactory gattoFactory = GattoFactory.getInstance();
+
+        //Creazione Post
+        Post post1 = new Post();
+        post1.setContent("Ciao, miei schiavi. Datemi cibo! Adesso! Miaomiaomiaomiaomiao!");
+        post1.setId(0);
+        post1.setUser(gattoFactory.getGattoById(0));
+
+        Post post2 = new Post();
+        post2.setContent("img/djanni1.jpg");
+        post2.setId(1);
+        post2.setUser(gattoFactory.getGattoById(0));
+        post2.setPostType(Post.Type.IMAGE);
+
+        Post post3 = new Post();
+        post3.setContent("img/djanni2.jpg");
+        post3.setId(2);
+        post3.setUser(gattoFactory.getGattoById(0));
+        post3.setPostType(Post.Type.IMAGE);
+
+        Post post4 = new Post();
+        post4.setContent("I need ansioliticy");
+        post4.setId(3);
+        post4.setUser(gattoFactory.getGattoById(1));
+
+        Post post5 = new Post();
+        post5.setContent("https://68.media.tumblr.com/51942e1f788f7209ee0f6db7cfc5e0fb/tumblr_n37ycpbMZf1rkxod7o1_500.jpg");
+        post5.setId(4);
+        post5.setUser(gattoFactory.getGattoById(1));
+        post5.setPostType(Post.Type.IMAGE);
+
+        listaPost.add(post1);
+        listaPost.add(post2);
+        listaPost.add(post3);
+        listaPost.add(post4);
+        listaPost.add(post5);
+    }
+
+    public Post getPostById(int id) {
+        for (Post post : this.listaPost) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
+        return null;
+    }
+
+    public List getPostList(Gatto gtt) {
+
+        List<Post> listaPost = new ArrayList<Post>();
+
+        for (Post post : this.listaPost) {
+            if (post.getUser().equals(gtt)) {
+                listaPost.add(post);
+            }
+        }
+        return listaPost;
+    }
+
 }
--- progettoAMM/src/java/amm/nerdbook/classi/UtentiregistratiFactory.java
+++ progettoAMM/src/java/amm/nerdbook/classi/UtentiregistratiFactory.java
@@ -4,11 +4,83 @@
  * and open the template in the editor.
  */
 package amm.nerdbook.classi;
-
+import java.util.ArrayList;
 /**
  *
  * @author Laura
  */
 public class UtentiregistratiFactory {
- 
+      //Pattern Design Singleton
+    private static UtentiregistratiFactory singleton;
+
+    public static UtentiregistratiFactory getInstance() {
+        if (singleton == null) {
+            singleton = new UtentiregistratiFactory();
+        }
+        return singleton;
+    }
+
+    private ArrayList<Utentiregistrati> listaUtentiregistrati = new ArrayList<Utentiregistrati>();
+
+    private UtentiregistratiFactory() {
+        //Creazione utenti
+
+        //Djanni
+        Utentiregistrati utente1 = new Utentiregistrati();
+        utente1.setId(0);
+        utente1.setNome("Djanni");
+        utente1.setEmail("djannigatto@gmail.com");
+        utente1.setCognome("Incrocio");
+        utente1.setPassword("123");
+        utente1.setUrlProfilo("img/djanniprofilo.jpg");
+        utente1.setFrase("");
+        utente1.setDataNascita("");
+
+        //HeavyBreathing
+        Utentiregistrati utente2 = new Utentiregistrati();
+        utente2.setId(0);
+        utente2.setNome("Djanni");
+        utente2.setEmail("djannigatto@gmail.com");
+        utente2.setCognome("Incrocio");
+        utente2.setPassword("123");
+        utente2.setUrlProfilo("img/djanniprofilo.jpg");
+        utente2.setFrase("");
+        utente2.setDataNascita("");
+
+        //GymWorkOut
+        Utentiregistrati utente3 = new Utentiregistrati();
+        utente3.setId(0);
+        utente3.setNome("Djanni");
+        utente3.setEmail("djannigatto@gmail.com");
+        utente3.setCognome("Incrocio");
+        utente3.setPassword("123");
+        utente3.setUrlProfilo("img/djanniprofilo.jpg");
+        utente3.setFrase("");
+        utente3.setDataNascita("");
+       
+        Utentiregistrati utente4 = new Utentiregistrati();
+        utente4.setId(0);
+        utente4.setNome("Djanni");
+        utente4.setEmail("djannigatto@gmail.com");
+        utente4.setCognome("Incrocio");
+        utente4.setPassword("123");
+        utente4.setUrlProfilo("img/djanniprofilo.jpg");
+        utente4.setFrase("");
+        utente4.setDataNascita("");
+
+        listaUtentiregistrati.add(utente1);
+        listaUtentiregistrati.add(utente2);
+        listaUtentiregistrati.add(utente3);
+        listaUtentiregistrati.add(utente4);
+    }
+
+    public Utentiregistrati getGattoById(int id) {
+        for (Utentiregistrati utente : this.listaUtentiregistrati) {
+            if (utente.getId() == id) {
+                return utente;
+            }
+        }
+        return null;
+    }
+
 }
